29b62f9c289a1a979ef3d28b72d33b6e97524e19
Henry Mosquera <henrym.718@hotmail.com>
2025-04-01 16:25:34 -0500

    Agregar hooks para el registro de usuarios: useActionRegisterForm para manejar la acción de registro y redirección, y useValidateRegisterForm para la validación del formulario utilizando Zod y react-hook-form.

---
+++ app/hooks/auth/register/useActionRegisterForm.tsx
@@ -0,0 +1,27 @@
+import { handleRegister } from "@/app/lib/actions/auth/register.action"
+import { RegisterType } from "@/app/lib/validation/register.schema"
+import { useRouter } from "next/navigation"
+import { startTransition, useActionState, useEffect } from "react"
+
+export default function useActionRegisterForm() {
+   const router = useRouter()
+   const [state, action, pending] = useActionState(handleRegister, { success: false, error: null })
+
+   const onSubmitHandler = (formData: RegisterType) => {
+      startTransition(() => {
+         action(formData)
+      })
+   }
+
+   useEffect(() => {
+      if (state?.success) {
+         router.push("/")
+      }
+   }, [state?.success, router])
+
+   return {
+      state,
+      pending,
+      onSubmitHandler,
+   }
+}
