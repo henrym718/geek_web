3a7b8d9c2ed3a4bca45e00b62333b8f58c623828
Henry Mosquera <henrym.718@hotmail.com>
2025-04-02 19:24:54 -0500

    Implementar comprobaciones de existencia de nombre de usuario y correo electrónico en el esquema de registro mediante la validación de Zod

--- app/lib/validation/register.schema.ts
+++ app/lib/validation/register.schema.ts
@@ -1,12 +1,30 @@
 import z from "zod"
-import { cityVO, emailVO, firstNameVO, lastNameVO, passwordVO, phoneVO } from "./values-objects"
+import { cityVO, emailVO, firstNameVO, lastNameVO, passwordVO, phoneVO, usernameVO } from "./values-objects"
+import { checkEmailExists, checkUsernameExists } from "@/app/services/auth/auth.services"
 
 export const registerSchema = z.object({
    firstName: firstNameVO,
    lastName: lastNameVO,
    city: cityVO,
    password: passwordVO,
-   email: emailVO,
+   username: usernameVO.superRefine(async (username, ctx) => {
+      const usernameExist = await checkUsernameExists({ username })
+      if (usernameExist.success && usernameExist.data.exists) {
+         ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Username ya existe, intenta con otro",
+         })
+      }
+   }),
+   email: emailVO.superRefine(async (email, ctx) => {
+      const emailExists = await checkEmailExists({ email })
+      if (emailExists.success && emailExists.data.exists) {
+         ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Parece que ya tiene una cuenta",
+         })
+      }
+   }),
    phone: phoneVO,
 })
 
