214ac514e89e22f4872b4a063a5dc13596f56231
Henry Mosquera <henrym.718@hotmail.com>
2025-04-06 12:59:06 -0500

    Implementar la función handleCreateProfile en el componente FooterCreateProfile para gestionar la creación de perfiles de proveedores, incluyendo la navegación y el reinicio del estado tras la creación exitosa.

--- app/components/features/vendor/create-profile/FooterCreateProfile.tsx
+++ app/components/features/vendor/create-profile/FooterCreateProfile.tsx
@@ -4,10 +4,27 @@ import { ProgressBar } from "@/app/components/ui/ProgressBar"
 import { Box } from "@/app/components/ui"
 import { useWizardCreateProfileStepHandlerStore } from "@/app/stores/vendor/wizard-create-profile-step-handler.store"
 import { useWizardUserDataStore } from "@/app/stores/vendor/wizard-create-profile-user-data.store"
+import { createVendorProfile } from "@/app/services/vendor/vendor.services"
+import { useAuthStore } from "@/app/stores/global/auth.store"
+import { useRouter } from "next/navigation"
 
 export default function FooterCreateProfile() {
-   const { step, totalSteps, previousStep, nextStep } = useWizardCreateProfileStepHandlerStore((state) => state)
-   const { vendorProfile } = useWizardUserDataStore((state) => state)
+   const router = useRouter()
+   const { step, totalSteps, previousStep, nextStep, resetStep } = useWizardCreateProfileStepHandlerStore((state) => state)
+   const { vendorProfile, resetVendorProfile } = useWizardUserDataStore((state) => state)
+   const { user } = useAuthStore((state) => state)
+
+   const handleCreateProfile = async () => {
+      if (!user?.vendor) return
+      console.log(vendorProfile, "vendorProfile")
+      const response = await createVendorProfile(vendorProfile)
+      if (response.success) {
+         resetVendorProfile()
+         resetStep()
+         router.push(`/vendor/${user.user.username}/profiles`)
+      }
+   }
+
    return (
       <Box className="flex flex-col gap-4 pb-4">
          <ProgressBar
@@ -24,15 +41,22 @@ export default function FooterCreateProfile() {
                Previous
             </Button>
 
-            <Button
-               data-step={step < totalSteps}
-               className="data-[step=false]:invisible"
-               variant="primary"
-               size="lg"
-               onClick={() => nextStep()}
-               disabled={(step === 1 && !vendorProfile.categoryId) || (step === 2 && !vendorProfile.skills.length)}>
-               Next
-            </Button>
+            {step === totalSteps ? (
+               <Button
+                  variant="primary"
+                  size="lg"
+                  onClick={handleCreateProfile}>
+                  Crear perfil
+               </Button>
+            ) : (
+               <Button
+                  variant="primary"
+                  size="lg"
+                  onClick={() => nextStep()}
+                  disabled={(step === 1 && !vendorProfile.categoryId) || (step === 2 && !vendorProfile.skills.length)}>
+                  Next
+               </Button>
+            )}
          </Box>
       </Box>
    )
