2c77d08c21338603c2ad8e5a89cbc3d0b9fb8353
Henry Mosquera <henrym.718@hotmail.com>
2025-04-06 12:57:19 -0500

    Implemente las funciones getVendorProfiles y createVendorProfile para manejar la recuperación y creación de perfiles de proveedores, mejorando la interacción de la API con un manejo de errores mejorado.

--- app/services/vendor/vendor.services.ts
+++ app/services/vendor/vendor.services.ts
@@ -5,18 +5,29 @@ import { ApiResponse } from "@/app/types/dtos/api-response.types"
 import { AxiosError } from "axios"
 import { mapAxiosErrorToApiResponse } from "../api/api.error-adapter"
 import { GetVendorProfileResponse } from "@/app/types/dtos/get-vendor-profile"
+import { CreateVendorProfileRequest, CreateVendorProfileResponse } from "@/app/types/dtos/create-vendor-profile.types"
 
 // Esta función obtiene el perfil del vendedor
 export const getVendorProfiles = async (accessToken: string): Promise<ApiResponse<GetVendorProfileResponse[]>> => {
    try {
       // Incluyendo el token manualmente en los headers
-      const response = await apiClient.get("/vendor-profile/me", {
+      const { data } = await apiClient.get<ApiResponse<GetVendorProfileResponse[]>>("/vendor-profile/me", {
          headers: {
             Authorization: `Bearer ${accessToken}`, // Agregamos el token en las cabeceras
          },
       })
 
-      return response.data
+      return data
+   } catch (error) {
+      return mapAxiosErrorToApiResponse(error as AxiosError)
+   }
+}
+
+export const createVendorProfile = async (vendorProfile: CreateVendorProfileRequest): Promise<ApiResponse<CreateVendorProfileResponse>> => {
+   try {
+      const { data } = await apiClient.post<ApiResponse<CreateVendorProfileResponse>>("/vendor-profile", vendorProfile)
+
+      return data
    } catch (error) {
       return mapAxiosErrorToApiResponse(error as AxiosError)
    }
