59bad3782ed85a9cd4b5cb8a1a17960081a82d4f
Henry Mosquera <henrym.718@hotmail.com>
2025-04-06 10:34:07 -0500

    Incorpore el componente ProfileHeadline al asistente de creación de perfiles de proveedores para mejorar la entrada del usuario para los títulos de perfil y el texto de orientación.

--- app/components/features/vendor/create-profile/WizardCreateProfile.tsx
+++ app/components/features/vendor/create-profile/WizardCreateProfile.tsx
@@ -1,5 +1,6 @@
 "use client"
 import GroupAndCategorySelector from "./GroupAndCategorySelector"
+import ProfileHeadline from "./ProfileHeadline"
 import SkillsSelector from "./SkillsSelector"
 import { useWizardCreateProfileStepHandlerStore } from "@/app/stores/vendor/wizard-create-profile-step-handler.store"
 
@@ -9,6 +10,7 @@ export default function WizardCreateProfile() {
    const RenderStep: Record<number, React.ReactElement> = {
       1: <GroupAndCategorySelector />,
       2: <SkillsSelector />,
+      3: <ProfileHeadline />,
    }
 
    return RenderStep[step]
