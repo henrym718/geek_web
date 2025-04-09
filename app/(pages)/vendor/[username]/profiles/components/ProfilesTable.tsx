05ffe05453fc2d3b3f4e7b18f436d637c222e538
Henry Mosquera <henrym.718@hotmail.com>
2025-04-09 11:22:44 -0500

    Agregar el componente ProfilesTable para mostrar una tabla de perfiles de proveedores, incluyendo funcionalidades para crear y visualizar perfiles, mejorando la gestión de datos en la aplicación.

---
+++ app/(pages)/vendor/[username]/profiles/components/ProfilesTable.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { Box, Button } from "@/app/components/ui"
+import { useRouter } from "next/navigation"
+import { GetVendorProfileResponse } from "@/app/data/dtos/get-vendor-profile"
+interface ProfilesTableProps {
+   data: GetVendorProfileResponse[]
+   username: string
+}
+
+const ProfilesTable: React.FC<ProfilesTableProps> = ({ data, username }) => {
+   const router = useRouter()
+   return (
+      <Box className="flex flex-col gap-4 w-1/2 mx-auto">
+         <Box className="flex justify-end pr-6">
+            <Button
+               variant="outline"
+               rounded="sm"
+               onClick={() => router.push(`/vendor/${username}/create/`)}>
+               Crear perfil
+            </Button>
+         </Box>
+         <table className="w-full text-left">
+            <thead>
+               <tr>
+                  <th>Título</th>
+                  <th>Categoría</th>
+                  <th>Fecha de Creación</th>
+                  <th>Fecha de Actualización</th>
+                  <th>Acciones</th>
+               </tr>
+            </thead>
+            <tbody>
+               {data.map((profile) => (
+                  <tr key={profile.id}>
+                     <td>{profile.title}</td>
+                     <td>{profile.category.name}</td>
+                     <td> {new Date(profile.createdAt).toLocaleDateString("es-ES")}</td>
+                     <td> {new Date(profile.updatedAt).toLocaleDateString("es-ES")}</td>
+                     <td>
+                        <Button
+                           variant="outline"
+                           rounded="sm">
+                           Ver
+                        </Button>
+                     </td>
+                  </tr>
+               ))}
+            </tbody>
+         </table>
+      </Box>
+   )
+}
+
+export default ProfilesTable
