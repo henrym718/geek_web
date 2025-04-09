d7aa74ce9f2fe62ec8cc67a96798083884fc5a6e
Henry Mosquera <henrym.718@hotmail.com>
2025-04-09 11:23:50 -0500

    Refactorizar el componente Login para simplificar la estructura de retorno, eliminando el contenedor innecesario y mejorando la legibilidad del código.

--- app/(pages)/(auth)/login/page.tsx
+++ app/(pages)/(auth)/login/page.tsx
@@ -1,9 +1,5 @@
 import { LoginForm } from "@/app/components/features/auth/login/LoginForm"
 
 export default function Login() {
-   return (
-      <div>
-         <LoginForm />
-      </div>
-   )
+   return <LoginForm />
 }
