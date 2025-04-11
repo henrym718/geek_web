221dbcfda6e342de015937e63ce7d998e5f8e4de
Henry Mosquera <henrym.718@hotmail.com>
2025-04-11 17:00:28 -0500

    Refactor proforma response service to update DTO imports and add checkResponseExists function for verifying existing responses. Adjusted createProformaResponse method to use new request structure, enhancing API interaction and data management.

--- src/services/proforma-response.service.ts
+++ src/services/proforma-response.service.ts
@@ -1,20 +1,26 @@
 import { ApiResponse } from "@/data/dtos/api-response.types"
-import { CreateProformaResponse, CreateProformaResponseRequest } from "@/data/dtos/create-proforma-response"
+import { CheckResponseExistsRes, CheckResponseExistsReq } from "@/data/dtos/check-reaponse-exists"
+import { CreateResponse, CreateResponseRequest } from "@/data/dtos/create-proforma-response"
 import { apiClient } from "@/lib/api/api.client"
 import { mapAxiosErrorToApiResponse } from "@/lib/api/api.error-adapter"
 import { AxiosError } from "axios"
 
-export const createProformaResponse = async (
-   accessToken: string,
-   request: CreateProformaResponseRequest
-): Promise<ApiResponse<CreateProformaResponse>> => {
+export const createProformaResponse = async (accessToken: string, request: CreateResponseRequest): Promise<ApiResponse<CreateResponse>> => {
    try {
       await new Promise((resolve) => setTimeout(resolve, 1000))
-      const { data } = await apiClient.post<ApiResponse<CreateProformaResponse>>("/proforma-response", request, {
-         headers: {
-            Authorization: `Bearer ${accessToken}`,
-         },
+      const { data } = await apiClient.post<ApiResponse<CreateResponse>>("/proforma-response", request, {
+         headers: { Authorization: `Bearer ${accessToken}` },
       })
+      return data
+   } catch (error) {
+      return mapAxiosErrorToApiResponse(error as AxiosError)
+   }
+}
+
+export const checkResponseExists = async (request: CheckResponseExistsReq): Promise<ApiResponse<CheckResponseExistsRes>> => {
+   try {
+      const { proformaRequestId, profileVendorId } = request
+      const { data } = await apiClient.get<ApiResponse<CheckResponseExistsRes>>(`/proforma-response/exists/${proformaRequestId}/${profileVendorId}`)
       return data
    } catch (error) {
       return mapAxiosErrorToApiResponse(error as AxiosError)
