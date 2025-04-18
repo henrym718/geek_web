c4e28d9b75908f44c9d6c122d6a2f78425f1e1a8
Henry Mosquera <henrym.718@hotmail.com>
2025-04-18 08:36:08 -0500

    Se integraron nuevas funcionalidades en el componente RequestScope, incluyendo la gestión del estado de selección de alcance mediante el uso de tiendas de datos. Se actualizaron las opciones de alcance con nuevos identificadores únicos y se implementó un manejador para la selección de alcance, mejorando la interactividad y la gestión de datos en el proceso de creación de solicitudes.

--- src/components/client/create-request/request-scope.tsx
+++ src/components/client/create-request/request-scope.tsx
@@ -1,10 +1,20 @@
 "use client"
 
 import { Box, InputSearch, Typography } from "@/components/ui"
+import { useCreateRequestUserDataStore } from "@/stores/use-create-request-user-data.store"
 import { useForm } from "react-hook-form"
+import { useCreateRequestFormDataStore } from "@/stores/use-create-request-form-data.store"
 
 export function RequestScope() {
+   const { setRequestData } = useCreateRequestUserDataStore((state) => state)
+   const { selectedScope, setSelectedScope } = useCreateRequestFormDataStore((state) => state)
+
    const { control } = useForm()
+
+   const handleScopeSelected = (option: { id: string; label: string }) => {
+      setRequestData({ scope: option.id })
+      setSelectedScope(option.label)
+   }
 
    return (
       <Box className="flex flex-col gap-2 w-4/7">
@@ -14,13 +24,15 @@ export function RequestScope() {
             label="Alcance"
             placeholder="Describe el alcance de la solicitud"
             options={[
-               { id: "1", label: "El Piedrero" },
-               { id: "2", label: "El Triunfo" },
-               { id: "3", label: "Pueblo Nuevo" },
-               { id: "4", label: "San pedro" },
+               { id: "09a48491-e0f4-4f56-9194-59c4f8d02084", label: "El Piedrero" },
+               { id: "09a48491-e0f4-4f56-9194-59c4f8d02085", label: "El Triunfo" },
+               { id: "09a48491-e0f4-4f56-9194-59c4f8d02086", label: "Pueblo Nuevo" },
+               { id: "09a48491-e0f4-4f56-9194-59c4f8d02087", label: "San pedro" },
             ]}
             name="scope"
             control={control}
+            onSelected={handleScopeSelected}
+            value={selectedScope}
          />
       </Box>
    )
