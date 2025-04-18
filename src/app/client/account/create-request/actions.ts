62af7eb5650a91148ce3e846657dfa7937d4c76f
Henry Mosquera <henrym.718@hotmail.com>
2025-04-18 08:35:52 -0500

    Se creó el archivo actions.ts para implementar la función createRequestAction, que gestiona la creación de solicitudes utilizando un token de acceso y datos de formulario. Esta adición mejora la funcionalidad del proceso de creación de solicitudes al integrar la lógica de manejo de errores y redirección tras una solicitud exitosa.

---
+++ src/app/client/account/create-request/actions.ts
@@ -0,0 +1,22 @@
+"use server"
+import { createRequest } from "@/data/api/services/proforma-request.service"
+import { CreateRequestRequest } from "@/data/dtos/create-request.type"
+import { getAccessTokenFromCookie } from "@/lib/utils/get-access-token-from-cookie"
+import { sleep } from "@/lib/utils/sleep"
+import { redirect } from "next/navigation"
+
+export async function createRequestAction(prevState: { success: boolean; error: string }, formData: FormData) {
+   const token = await getAccessTokenFromCookie()
+   if (!token) return { success: false, error: "No se pudo obtener el token de acceso" }
+
+   const payloadJson = formData.get("payload") as string
+   const requestData: CreateRequestRequest = JSON.parse(payloadJson)
+
+   await sleep(3000)
+   const response = await createRequest(requestData, token)
+   if (response.success) {
+      redirect("/")
+   }
+
+   return { success: false, error: response.message }
+}
