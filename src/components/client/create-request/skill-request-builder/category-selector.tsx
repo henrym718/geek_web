ad1e67215f39640ca14747129b74e8b6f2c09ba7
Henry Mosquera <henrym.718@hotmail.com>
2025-04-18 08:36:36 -0500

    Se añadió la integración de la tienda useCreateRequestUserDataStore en el componente CategorySelector para gestionar el estado de la categoría seleccionada y actualizar los datos de la solicitud. Se mejoró la lógica de selección de categorías al establecer el ID de la categoría en el estado de la solicitud.

--- src/components/client/create-request/skill-request-builder/category-selector.tsx
+++ src/components/client/create-request/skill-request-builder/category-selector.tsx
@@ -3,6 +3,7 @@
 import { SelectButton, SelectButtonContent, SelectButtonItem, SelectButtonTrigger, SelectButtonValue } from "@/components/ui"
 import { fetchCategoriesByGroupId } from "@/data/api/services/category.service"
 import { useCreateRequestFormDataStore } from "@/stores/use-create-request-form-data.store"
+import { useCreateRequestUserDataStore } from "@/stores/use-create-request-user-data.store"
 import { useEffect, useMemo } from "react"
 import useSWR from "swr"
 
@@ -10,7 +11,7 @@ export function CategorySelector() {
    const { categories, selectedGroup, selectedCategory, setCategories, setSelectedCategory, cleanSkillsSelected } = useCreateRequestFormDataStore(
       (state) => state
    )
-
+   const { setRequestData } = useCreateRequestUserDataStore((state) => state)
    const categorySWRKey = selectedGroup?.id ? `categories/${selectedGroup.id}` : null
    const { data: categoriesApiResponse, isLoading: isLoadingCategories } = useSWR(categorySWRKey, () => fetchCategoriesByGroupId(selectedGroup.id))
 
@@ -26,11 +27,12 @@ export function CategorySelector() {
       if (!value) return
       setSelectedCategory(value)
       cleanSkillsSelected()
+      setRequestData({ categoryId: value.id })
    }
 
    return (
       <SelectButton
-         value={selectedCategory}
+         selected={selectedCategory}
          onChange={handleCategorySelect}>
          <SelectButtonTrigger>
             <SelectButtonValue placeholder="Seleccione una categoría" />
