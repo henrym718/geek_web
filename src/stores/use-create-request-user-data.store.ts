a7b765d64a673c4eaec07cf54cd43170a07848d2
Henry Mosquera <henrym.718@hotmail.com>
2025-04-18 08:35:25 -0500

    Se añadieron nuevos métodos y propiedades a la tienda useCreateRequestUserDataStore para gestionar habilidades y restablecer datos de solicitud. Esto mejora la funcionalidad y la flexibilidad en el proceso de creación de solicitudes.

--- src/stores/use-create-request-user-data.store.ts
+++ src/stores/use-create-request-user-data.store.ts
@@ -1,9 +1,13 @@
 import { CreateRequestRequest } from "@/data/dtos/create-request.type"
+import { Skill } from "@/data/types/models/models"
 import { create } from "zustand"
 
 interface CreateRequestUserDataStore {
    requestData: CreateRequestRequest
    setRequestData: (requestData: Partial<CreateRequestRequest>) => void
+   resetRequestData: () => void
+   addSkill: (skill: Skill) => void
+   removeSkill: (skill: Skill) => void
 }
 
 export const useCreateRequestUserDataStore = create<CreateRequestUserDataStore>((set) => ({
@@ -11,12 +15,33 @@ export const useCreateRequestUserDataStore = create<CreateRequestUserDataStore>(
       title: "",
       description: "",
       budget: 0,
-      budgetUnit: "project",
+      budgetUnit: "PROJECT",
       quotation: false,
       scope: "",
-      clientId: "",
       categoryId: "",
       skills: [],
+      projectType: null,
+      projectLength: null,
+      projectWorkload: null,
    },
    setRequestData: (requestData: Partial<CreateRequestRequest>) => set((state) => ({ requestData: { ...state.requestData, ...requestData } })),
+   resetRequestData: () =>
+      set({
+         requestData: {
+            title: "",
+            description: "",
+            budget: 0,
+            budgetUnit: "PROJECT",
+            quotation: false,
+            scope: "",
+            categoryId: "",
+            skills: [],
+            projectType: null,
+            projectLength: null,
+            projectWorkload: null,
+         },
+      }),
+   addSkill: (skill: Skill) => set((state) => ({ requestData: { ...state.requestData, skills: [...state.requestData.skills, skill.id] } })),
+   removeSkill: (skill: Skill) =>
+      set((state) => ({ requestData: { ...state.requestData, skills: state.requestData.skills.filter((s) => s !== skill.id) } })),
 }))
