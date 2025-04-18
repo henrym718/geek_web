87b4239719908b3eb9aa2a363f8964109b44c42f
Henry Mosquera <henrym.718@hotmail.com>
2025-04-18 08:34:11 -0500

    Agregue la función createRequest a proforma-request.service para la creación de solicitudes

--- src/data/api/services/proforma-request.service.ts
+++ src/data/api/services/proforma-request.service.ts
@@ -1,8 +1,15 @@
 import { ApiResponse } from "@/data/dtos/api-response.types"
 import { GetRequestByProfileIdResponse } from "@/data/dtos/get-request-by-vendor-profile-id"
 import { PROFORMA_REQUEST_ENDPOINTS } from "@/config/constants"
-import { apiGet } from "../api.client"
+import { apiGet, apiPost } from "../api.client"
+import { CreateRequestRequest, CreateRequestResponse } from "@/data/dtos/create-request.type"
 
 export async function fetchProformaRequestByProfileId(vendorProfileId: string): Promise<ApiResponse<GetRequestByProfileIdResponse[]>> {
    return await apiGet<GetRequestByProfileIdResponse[]>(PROFORMA_REQUEST_ENDPOINTS.PROFORMA_REQUEST_BY_VENDOR_PROFILE_ID(vendorProfileId))
+}
+
+export async function createRequest(requestData: CreateRequestRequest, token: string): Promise<ApiResponse<CreateRequestResponse>> {
+   return await apiPost<CreateRequestResponse, CreateRequestRequest>(PROFORMA_REQUEST_ENDPOINTS.CREATE_REQUEST, requestData, {
+      headers: { Authorization: `Bearer ${token}` },
+   })
 }
